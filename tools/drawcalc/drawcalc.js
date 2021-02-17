2b3ee8e2e230a9d5741492436603d9f624e59518
WingTSK <wingtsk@gmail.com>
2021-02-17 13:09:52 +0900

    drawcalc pwa-test fix

--- tools/drawcalc/drawcalc.js
+++ tools/drawcalc/drawcalc.js
@@ -10,7 +10,7 @@
   'use strict';
   
   const PRODUCT = 'drawcalc';
-  const VERSION = '0.5.0';
+  const VERSION = '0.5.1';
   const AUTHOR = 'WingTSK';
   
   if (!root[PRODUCT]) root[PRODUCT] = {};
